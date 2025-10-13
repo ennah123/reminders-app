890aed5d4576da622594de024bc4d035e7cdcce1
ennah123 <123ennah@gmail.com>
2025-10-13 23:25:18 +0100

    first try to integrate next auth with google

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {SignIn} from './sign-in'
 
 const Header = () => {
   return (
@@ -12,7 +13,8 @@ const Header = () => {
             <nav className="hidden md:flex space-x-8">
               <a href="#" className="text-gray-900 font-medium text-sm">Dashboard</a>
               <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Calendar</a>
-              <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Settings</a>
+              {/* <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Settings</a> */}
+              <SignIn/>
             </nav>
           </div>
         </div>
