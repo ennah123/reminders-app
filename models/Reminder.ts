3d6397bd6ab524317a11a272db047e558a6aedaa
ennah123 <123ennah@gmail.com>
2025-10-17 00:46:05 +0100

    add the user email in the reminder

--- models/Reminder.ts
+++ models/Reminder.ts
@@ -13,10 +13,16 @@ const reminderSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  user_email: { 
+    type: String,
+    required: true
+  },
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
+// delete mongoose.models.Reminder;
+// delete mongoose.connection.collections.reminders;
 
 export default mongoose.models.Reminder || mongoose.model('Reminder', reminderSchema);
